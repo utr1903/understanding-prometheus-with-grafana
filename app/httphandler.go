686d5c3d99dc196a4cf7af9f4e15b2abaa2dfb82
uturkarslan <ugurturkarslan@gmail.com>
2023-07-05 11:06:37 +0300

    Add gauge

--- app/httphandler.go
+++ app/httphandler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -25,6 +26,18 @@ var (
 			LABEL_USER,
 		},
 	)
+
+	gauge = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "my_app_http_requests_in_process",
+			Help: "Total amount of HTTP requests being processed at the time",
+		},
+		[]string{
+			LABEL_METHOD,
+			LABEL_STATUS_CODE,
+			LABEL_USER,
+		},
+	)
 )
 
 // HTTP handler
@@ -33,18 +46,25 @@ func httpHandler(
 	r *http.Request,
 ) {
 
+	// Extract query parameters
 	statusCodeParam := r.URL.Query().Get(LABEL_STATUS_CODE)
 	user := r.URL.Query().Get(LABEL_USER)
 
+	// Increase in-process requests
+	updateInProcessRequests(true, r.Method, statusCodeParam, user)
+
+	// Wait 2 seconds
+	time.Sleep(2 * time.Second)
+
+	// Write response
 	w.WriteHeader(getCorrespondingStatusCode(statusCodeParam))
 	w.Write([]byte("Request is handled."))
 
-	counter.With(
-		prometheus.Labels{
-			LABEL_METHOD:      r.Method,
-			LABEL_STATUS_CODE: statusCodeParam,
-			LABEL_USER:        user,
-		}).Inc()
+	// Decrease in-process requests
+	updateInProcessRequests(false, r.Method, statusCodeParam, user)
+
+	// Increment request counter
+	incrementRequestCounter(r.Method, statusCodeParam, user)
 }
 
 func getCorrespondingStatusCode(
@@ -60,4 +80,40 @@ func getCorrespondingStatusCode(
 	default:
 		return http.StatusInternalServerError
 	}
+}
+
+func updateInProcessRequests(
+	increment bool,
+	method string,
+	statusCode string,
+	user string,
+) {
+	if increment {
+		gauge.With(
+			prometheus.Labels{
+				LABEL_METHOD:      method,
+				LABEL_STATUS_CODE: statusCode,
+				LABEL_USER:        user,
+			}).Inc()
+	} else {
+		gauge.With(
+			prometheus.Labels{
+				LABEL_METHOD:      method,
+				LABEL_STATUS_CODE: statusCode,
+				LABEL_USER:        user,
+			}).Dec()
+	}
+}
+
+func incrementRequestCounter(
+	method string,
+	statusCode string,
+	user string,
+) {
+	counter.With(
+		prometheus.Labels{
+			LABEL_METHOD:      method,
+			LABEL_STATUS_CODE: statusCode,
+			LABEL_USER:        user,
+		}).Inc()
 }
